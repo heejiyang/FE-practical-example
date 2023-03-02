2335790f5e028f7c607d3ac40852aaa947e62143
heejiyang <dianoheeji@naver.com>
2023-03-02 18:53:59 +0900

    accordion JS: FAQ 제목 클릭 이벤트

--- 아코디언/accordion.js
+++ 아코디언/accordion.js
@@ -1,4 +1,12 @@
 window.onload = () => {
   const panelFaqComtainer = document.querySelectorAll(".panel-faq-container");
   console.log(panelFaqComtainer); // NodeList 객체
+
+  // 반복문 순회하며 해당 FAQ제목 클릭시 콜백처리
+  for (let i = 0; i < panelFaqComtainer.length; i++) {
+    panelFaqComtainer[i].addEventListener("click", function () {
+      // 클릭시 처리할 일
+      console.log("나 클릭했음!!" + i);
+    });
+  }
 };
