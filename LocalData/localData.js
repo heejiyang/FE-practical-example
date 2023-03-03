317e28502e29b8a1baa69b494b7ecc98d16e69e9
heejiyang <dianoheeji@naver.com>
2023-03-03 15:25:48 +0900

    localData HTML, CSS

--- LocalData/localData.js
+++ LocalData/localData.js
@@ -1,5 +1,20 @@
-// 브라우저 로컬 데이터에 저장하기
+window.onload = () => {
+  // SetData 버튼
+  const btnSLD = document.querySelectorAll(".btnSetLocalData");
+  console.log(btnSLD);
 
+  // Input 텍스트 값
+  const input = document.querySelectorAll("input");
+
+  // Set Data 버튼 클릭 시
+  btnSLD.addEventListener("click", () => {
+    // 할 일 처리 -> 입력한 텍스트 값 가져오기
+    const inputValue = input.value;
+    console.log(inputValue);
+  });
+};
+
+// 브라우저 로컬 데이터에 저장하기
 // 주요 메서트 2개
 // 값 저장하기 localStorage.setItem('key', value)
 // 값 가져오기 localStorage.getItem('key')
