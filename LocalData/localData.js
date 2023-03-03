672ee0b83c7a22ecaf0f68db3934bf0b3726d001
heejiyang <dianoheeji@naver.com>
2023-03-03 16:19:01 +0900

    localData Js: localData 가져오기 기능 추가

--- LocalData/localData.js
+++ LocalData/localData.js
@@ -1,16 +1,35 @@
 window.onload = () => {
   // SetData 버튼
-  const btnSLD = document.querySelectorAll(".btnSetLocalData");
+  const btnSLD = document.querySelector(".btnSetLocalData");
   console.log(btnSLD);
 
   // Input 텍스트 값
-  const input = document.querySelectorAll("input");
+  const input = document.querySelector("input");
 
   // Set Data 버튼 클릭 시
   btnSLD.addEventListener("click", () => {
     // 할 일 처리 -> 입력한 텍스트 값 가져오기
     const inputValue = input.value;
     console.log(inputValue);
+
+    // localStorage에 저장하기
+    localStorage.setItem("userId", inputValue);
+    input.value = "";
+  });
+
+  // Get Data 버튼 클릭 시
+  const btnGLD = document.querySelector(".btnGetLocalData");
+  console.log(btnGLD);
+
+  btnGLD.addEventListener("click", () => {
+    // 할 일 처리 -> 해당 키의 로컬 데이터가 있으면 가져오기
+    const getData = localStorage.getItem("userId");
+    if (!getData) {
+      alert("해당 키로 로컬에 저장된 데이터가 없다.");
+    } else {
+      // input 텍스트 박스에 삽입
+      input.value = getData;
+    }
   });
 };
 
