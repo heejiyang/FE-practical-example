6dc7d1bf9951891790424a9debac23c0f77fb50f
heejiyang <dianoheeji@naver.com>
2023-03-09 21:47:39 +0900

    cookie JS : 쿠키  생성하기

--- Cookie/cookie.js
+++ Cookie/cookie.js
@@ -17,3 +17,32 @@ function getCookie() {
     alert("저장된 쿠키가 없습니다. ₩n(첫 방문을 환영합니다.)");
   }
 }
+
+// cookie 생성하기
+function setCookie() {
+  // 기본적인 날짜 출력 -> 날짜 쓸 일 -> 만료일 -> expiration, expire
+  // 로그인 유지 기간 조절 목적으로 쓰이기도 한다.
+  // let nowDate = new Date();
+  // console.log(nowDate); // type: object
+
+  // 만료일 생성
+  // let expiration = nowDate.getDate() + 10;
+  // console.log(expiration); // 오늘 날짜 + 10일 뒤
+
+  // 오늘 날짜 -> 10일 뒤 날짜가 나타나도록 생성
+  let expiration = new Date();
+  expiration.setDate(expiration.getDate() + 10);
+  console.log(expiration); // Sun Mar 19 2023 21:41:27 GMT+0900 (한국 표준시)
+
+  // 날짜를 쿠키로 저장하기 위해 -> UTC방식으로 표기 -> toUTCString() 메서드 사용
+  console.log(expiration.toUTCString()); // Sun, 19 Mar 2023 12:41:27 GMT
+
+  // 쿠키 생성하기
+  let cookies = "";
+  cookies = "userid = superman; expires = expiration.toUTCString()";
+  console.log(cookies);
+
+  // 쿠키 저장하기
+  document.cookie = cookies;
+  alert("쿠키를 생성하였습니다.");
+}
